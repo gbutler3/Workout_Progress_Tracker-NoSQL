17d697a65f99b9ad17577118e51930b01346bba9
grace butler <gmx.butler@gmail.com>
2021-09-05 15:08:09 -0500

    working on routes with HTML pages loading correctly.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,13 @@
 const router = require("express").Router();
 
-
+router.get("/api/workouts", (req, res) => {
+  database.Workout.find()
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.status(400).json(err)
+  })
+})
 
 module.exports = router;
