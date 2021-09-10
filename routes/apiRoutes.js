19657f57e00936ca891fec0868a53481699494e8
grace butler <gmx.butler@gmail.com>
2021-09-10 11:54:12 -0500

    working on how the charts show up

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -27,7 +27,11 @@ router.put("/api/workouts/:id", (req, res) => {
   const id = req.params.id;
   const body = req.body;
 
-  workouts.Workout.findOneAndUpdate({ _id: id }, { $push: { exercises: body } })
+  workouts.Workout.findOneAndUpdate(
+    { _id: id }, 
+    { $push: { exercises: body }}, 
+    { new: true, runValidators: true} //this is so it doesn't log a new workout for every exercise you put in on one day. It adds to duration, weight ect. 
+    )
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
@@ -39,6 +43,8 @@ router.put("/api/workouts/:id", (req, res) => {
 
 router.get("/api/workouts/range", (req, res) => {
   workouts.Workout.find({})
+  .sort({$natural: -1})// shows the last 7 days. Only issue is it shows the chart backwards
+  .limit(7) //this limits the data so that it shows 7 workouts
   .then(dbWorkout => {
     res.json(dbWorkout);
   })
